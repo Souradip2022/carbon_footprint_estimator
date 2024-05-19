2a77c81f88043ba43732d883a09b1d4b4b31df5d
Souradip2022 <22052939@kiit.ac.in>
2024-05-19 23:54:38 +0530

    Added cards for all models and containing data inside the card using data json

--- web_client/src/components/available_models/NightLamp.tsx
+++ web_client/src/components/available_models/NightLamp.tsx
@@ -2,10 +2,30 @@
 import React from "react";
 import {motion} from "framer-motion";
 import {LampContainer} from "../ui/lamp";
+import {BackgroundGradient} from "@/components/ui/background-gradient";
+import modelData from "@/data/modelData.json"
+import Link from "next/link";
+import Image from "next/image";
+import {generateKey} from "node:crypto";
+import {Button} from "@/components/ui/button";
+
+interface ModelData {
+  image: string;
+  name: string;
+  description: string;
+  slug: string;
+}
 
 export function NightLamp() {
+
+  const data: Array<ModelData> = modelData;
+
+  function generateRandomKey(): string {
+    return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+  }
+
   return (
-    <LampContainer className="absolute -top-14 h-fit
+    <LampContainer className="relative h-[80rem] w-full flex flex-col justify-center items-center
     ">
       <motion.h1
         initial={{opacity: 0.5, y: 100}}
@@ -15,10 +35,34 @@ export function NightLamp() {
           duration: 0.8,
           ease: "easeInOut",
         }}
-        className="mt-8 bg-gradient-to-br from-slate-300 to-slate-500 py-4 text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl"
-      >
-        Build lamps <br/> the right way
+        className="mt-8 bg-gradient-to-br from-slate-300 to-slate-500 py-4 text-center text-4xl bg-clip-text font-medium tracking-tight text-transparent md:text-7xl w-full absolute -top-[700px]">
+        List of all our available Models
       </motion.h1>
+
+      <div className="grid lg:grid-cols-3 gap-10 grid-cols-1 md:grid-cols-2 my-20 w-full absolute -top-[550px]">
+        {data.map(item =>
+          <div
+            className="flex items-center justify-center w-[100%] h-full"
+            key={generateRandomKey()}>
+            <BackgroundGradient
+              className="flex w-full flex-col gap-5 rounded-[22px] relative bg-zinc-900  items-center justify-around lg:min-h-[535px] py-3 px-4 lg:max-w-[500px]">
+              <Image
+                src={item.image} alt={item.name} height="225"
+                width="350" className="rounded-sm w-[350px] h-[225px]"/>
+
+              <h1 className="text- text-center text-2xl font-semibold tracking-tight ">{item.name}</h1>
+
+              <p className=" text-center text-lg">{item.description}</p>
+              <Link href={`all_models/${item.slug}`}>
+                <Button className="">{"Use Model"}</Button>
+              </Link>
+            </BackgroundGradient>
+          </div>
+        )}
+
+
+      </div>
+
     </LampContainer>
   );
 }
