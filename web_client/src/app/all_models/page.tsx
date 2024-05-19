7d3fddde2a5bef23efcc9761dc502ddd2c501143
Souradip2022 <22052939@kiit.ac.in>
2024-05-19 14:19:36 +0530

    Added NightLamp component to all_models route

--- web_client/src/app/all_models/page.tsx
+++ web_client/src/app/all_models/page.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import {NightLamp} from "@/components/available_models/NightLamp";
 
 function Page() {
   return (
-    <div>Hello</div>
+    <main className="flex min-h-screen flex-col items-center justify-center bg-primary-foreground gap-y-10">
+      <NightLamp/>
+    </main>
   );
 }
 
